d0ddf62dc911e28cc55654f95c36df9051de459c
Vivekpatel <vp20032020@gmail.com>
2025-03-04 12:13:21 +0530

    [Vivek] Added writeInWords.js

---
+++ ifelseifstatements/writeInWords.js
@@ -0,0 +1,21 @@
+const number = parseInt(process.argv[2]);
+if(number===1)
+    console.log("One");
+else if(number===2)
+    console.log("Two");
+else if(number===3)
+    console.log("Three");
+else if(number===4)
+    console.log("Four");
+else if(number===5)
+    console.log("Five");
+else if(number===6)
+    console.log("Six");
+else if(number===7)
+    console.log("Seven");
+else if(number===8)
+    console.log("Eigth");
+else if(number==9)
+    console.log("Nine");
+else
+    console.log("Enter a Vaild Single Digit Number");
