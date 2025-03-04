f4e8af018f89ef32e4a60b40a76ec68805161e9b
Vivekpatel <vp20032020@gmail.com>
2025-03-04 12:18:30 +0530

    [Vivek] Added displayDay.js

---
+++ ifelseifstatements/displayDay.js
@@ -0,0 +1,17 @@
+const number = parseInt(process.argv[2]);
+if(number===1)
+    console.log("Sunday");
+else if(number===2)
+    console.log("Monday");
+else if(number===3)
+    console.log("Tuesday");
+else if(number===4)
+    console.log("Wednesday");
+else if(number===5)
+    console.log("Thursday");
+else if(number===6)
+    console.log("Friday");
+else if(number===7)
+    console.log("Saturday");
+else
+    console.log("Enter a Vaild Day");
